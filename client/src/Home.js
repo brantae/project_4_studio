8727ead2ab6c36185dec1244ae57a39cc2bf52cc
Anslie Brant <brantae@gmail.com>
2023-08-08 22:32:58 -0500

    fixed issue of buttom appearing on same line

--- client/src/Home.js
+++ client/src/Home.js
@@ -11,9 +11,11 @@ function Home() {
         <h1>Welcome to The Studio!</h1>
         <h2>Book private lessons with various teachers in our state-of-the-art studio space.</h2>
         <h2>Log in or sign up to book a lesson with your preferred teacher today.</h2>
+        <div>
         <Button as={Link} to='/login' primary>
             click here to log in
         </Button>
+        </div>
     </div>
     
     )
